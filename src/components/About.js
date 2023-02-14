e0b09ddb16041020959311e8f6e2c0e9fdf33ff4
sarang <sarang.office01@gmail.com>
2023-02-14 13:51:21 +0530

    add some data

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-fit bg-gradient-to-b from-gray-800 to-black text-white '>
-      <div className='p-4 max-w-screen-lg  mx-auto flex flex-col justify-center w-full h-full '>
-        <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+    <div
+      name="about"
+      className="w-full h-fit bg-gradient-to-b from-gray-800 to-black text-white "
+    >
+      <div className="p-4 max-w-screen-lg  mx-auto flex flex-col justify-center w-full h-full ">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
-        <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore a excepturi veniam voluptatem quae ratione officiis, nobis totam, eaque perferendis error quis. Suscipit consequatur mollitia, ducimus adipisci dignissimos, error consectetur libero unde ratione temporibus voluptatem praesentium facilis dolor officiis iste.</p>
+        <p className="text-xl mt-20">
+          An Full Stack Developer with excellent technical knowledge and
+          problem-solving ability. An enthusiastic individual with highly
+          motivated and leadership skills. Ability to perform under pressure and
+          ready to meet deadlines. Has great adaptability to change and eager to
+          learn new technologies and methodologies.
+        </p>
 
         <br />
 
-        <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita iusto amet quas? Quisquam odit dignissimos, natus eligendi a quae. Eligendi totam officiis ratione nesciunt exercitationem, esse deserunt adipisci magnam temporibus. Harum quis aspernatur culpa neque! Culpa optio hic minima ea tempora blanditiis necessitatibus, vero quis labore adipisci consequatur assumenda!</p>
-
+        <p className="text-xl">
+          I have knowledge of Full Stack as well as Automation testing due to which
+          I am good at technology like HTML,CSS, Javascript,React,NodeJs,Express,Mongodb,Mongoose,
+          Git & Git-Hub,Tailwind,Postman,Sass,MySql and Due to Automation I also know 
+          Java,seleniun,TestNg etc...
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,52 @@
-import React from 'react'
-import HeroImage from '../assets/hero.jpg'
-import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
-import {Link} from 'react-scroll'
+import React from "react";
+import HeroImage from "../assets/hero.jpg";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name='home' className='h-screen w-full   bg-gradient-to-b from-black via-black to-gray-800  '>
-      <div className='mx-auto max-w-screen-lg flex flex-col justify-center items-center h-full px-4  md:flex-row'>
-        <div className='flex flex-col justify-center gap-1 h-full mt-12'>
-            <h2 className='text-4xl lg:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
-            <p className='py-4 text-gray-500 max-w-md'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id recusandae fuga consequuntur voluptatibus harum quia quibusdam dolore odio, saepe perferendis hic est a temporibus reprehenderit, aperiam aspernatur labore! Corporis, alias!</p>
+    <div
+      name="home"
+      className="h-screen w-full   bg-gradient-to-b from-black via-black to-gray-800  "
+    >
+      <div className="mx-auto max-w-screen-lg flex flex-col justify-center items-center h-full px-4  md:flex-row">
+        <div className="flex flex-col justify-center gap-1 h-full mt-12">
+          <h2 className="text-4xl lg:text-7xl font-bold text-white">
+            I'm a Full Stack Developer
+          </h2>
+          <p className="py-4 text-gray-500 max-w-md">
+            An Full Stack Developer with excellent technical knowledge and
+            problem-solving ability. An enthusiastic individual with highly
+            motivated and leadership skills. Ability to perform under pressure
+            and ready to meet deadlines. Has great adaptability to change and
+            eager to learn new technologies and methodologies.
+          </p>
 
-            <div>
-                <Link to='portfolio' smooth duration={500} className='group px-6 py-3 my-2 text-white w-fit flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
-                    Portfolio
-                    <span className='group-hover:rotate-90 duration-300'>
-                        <MdOutlineKeyboardArrowRight className='ml-1' size={25}/>
-                    </span>
-                </Link>
-            </div>
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group px-6 py-3 my-2 text-white w-fit flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight className="ml-1" size={25} />
+              </span>
+            </Link>
+          </div>
         </div>
 
-        <div className=''>
-            <img className='rounded-xl mx-auto w-2/3 md:w-full object-cover' src={HeroImage} alt="hero" />
+        <div className="">
+          <img
+            className="rounded-xl mx-auto w-2/3 md:w-full object-cover"
+            src={HeroImage}
+            alt="hero"
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
